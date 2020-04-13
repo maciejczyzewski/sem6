139ed9366162e9404bfb86c19ec7d06bb0432357
Maciej A. Czyzewski <maciejanthonyczyzewski@gmail.com>
2020-04-13 22:31:00 +0200

    add build

---
+++ precache-manifest.b1e625aff879e9a984a399fb1fb69116.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbccb4e570fdd61f9699ac4fbac20a92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70c8be05cd52df918a63",
+    "url": "/static/css/main.eb1bd558.chunk.css"
+  },
+  {
+    "revision": "9c36ec14cf5a8eb5db82",
+    "url": "/static/js/2.79f636e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.79f636e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70c8be05cd52df918a63",
+    "url": "/static/js/main.529f2c84.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "6a2998cf6526ce5bb3f24ac2103c9fad",
+    "url": "/static/media/doge.6a2998cf.svg"
+  },
+  {
+    "revision": "3d138fd1dda16709ce29a20651f3df02",
+    "url": "/static/media/help.3d138fd1.svg"
+  },
+  {
+    "revision": "023a90fe069158cfcd0c3ef1a5a51f4a",
+    "url": "/static/media/star-empty.023a90fe.svg"
+  },
+  {
+    "revision": "37ced0d0dd57c2ed836318b223ebb073",
+    "url": "/static/media/star-full.37ced0d0.svg"
+  }
+]);
